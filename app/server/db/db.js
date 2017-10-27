013e3a255c311032105f0cd7fb0dba9a7de85a1a
Ben Redden <benjamminredden@gmail.com>
2017-10-27 08:46:11 -0400

    making progress with Hapi/Nes/RethinkDB bones

---
+++ app/server/db/db.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const r = require('rethinkdb');
+
+exports.register = function (server, options, next) {
+  const db = 'complaints';
+  const code_snippets = 'code_snippet';
+  const users = 'users';
+
+  let conn;
+
+  r.connect({host: 'rethinkdb', port: 28015}, function(err, connection){
+    if (err) {
+      return next(err);
+    }
+
+    conn = connection;
+
+    r.dbCreate(db).run(connection, (err, result) => {
+      r.db(db).tableCreate(code_snippets).run(connection, (err, result) => {
+        return next();
+      });
+    });
+  });
+
+  server.method('db.saveEntry', (entry, callback) => {
+    r.db(db).table(code_snippets).insert(entry).run(conn, callback);
+  });
+
+  server.method('db.findEntries', (limit, callback) => {
+    r.db(db).table(code_snippets).orderBy(r.desc('createdAt')).limit(limit).run(conn, callback);
+  });
+
+  server.method('db.setupChangefeedPush', () => {
+    r.db(db).table(code_snippets).changes().run(conn, (err, cursor) => {
+      cursor.each((err, item) => {
+        server.publish('/complaints/updates', item.new_val);
+      });
+    });
+  }, {
+    callback: false
+  });
+};
+
+exports.register.attributes = {
+  name: 'db'
+};
