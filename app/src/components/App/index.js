6458896c09be64ac29134a96e283952474914db4
Ben Redden <benjamminredden@gmail.com>
2017-10-29 11:06:47 -0400

    work on websockets, getting everything synced up

--- app/src/components/App/index.js
+++ app/src/components/App/index.js
@@ -4,7 +4,7 @@ import nes from 'nes/client';
 import './style.css';
 
 //Setup the websocket connection and react to updates
-var client = new nes.Client('ws://localhost:9000');
+var client = new nes.Client('ws://172.18.0.1:9000');
 
 client.connect(function (err) {
   var handler = function (item) {
@@ -13,6 +13,35 @@ client.connect(function (err) {
 
   client.subscribe('/complaints/updates', handler, function (err) {});
 });
+
+const getComplaints = function() {
+  fetch('complaints').then(function(response) {
+    console.log('response is ',response);
+  }, function(error) {
+    console.log('error is ',error);
+  });
+}
+getComplaints();
+const addComplaint = function(data) {
+  console.log('adding ', data);
+  fetch('/timeline/createEntry', {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json"
+    },
+    credentials: "same-origin"
+  }).then(function(response) {
+    response.status     //=> number 100–599
+    response.statusText //=> String
+    response.headers    //=> Headers
+    response.url        //=> String
+
+    return response.text()
+  }, function(error) {
+    error.message //=> String
+  });
+}
 
 class App extends Component {
 
